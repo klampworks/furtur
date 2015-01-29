1a5416b9858a5b3b39025e500202bac2b10b616a
yukipyon <wolfvbear@gmail.com>
2015-01-29 19:01:07 +0000

    Split into base class and iw derivitive.

---
+++ wifi/Wifi_base.rb
@@ -0,0 +1,14 @@
+class Wifi_base
+
+    def connect()
+        return true
+    end
+
+    def disconnect()
+        return true
+    end
+
+    def parse_networks(s)
+    end
+end 
+
---
+++ wifi/Wifi_iw.rb
@@ -0,0 +1,38 @@
+require './Wifi_base'
+
+class Wifi_iw < Wifi_base
+
+    def connect()
+        return true
+    end
+
+    def disconnect()
+        return true
+    end
+
+    def parse_networks(s)
+        aps = Array.new
+        i = -1
+
+        s.each_line { |l|
+
+            if m = /^BSS (?<bssid>[0-9a-f:]+)/.match(l) then
+                i += 1
+                aps[i] = Hash.new
+                aps[i]['bssid'] = m.captures
+                aps[i]['enc'] = false
+            end
+
+            if m = /^\W*SSID: ([^\n]+)/.match(l) then
+                aps[i]['essid'] = m.captures
+            end
+                
+            if /^\W*RSN:/.match(l) then
+                aps[i]['enc'] = true
+            end
+        }
+
+        return aps
+    end
+end 
+
