5ed38beac1ced41b8a8974f2ac989bfa071b460a
yukipyon <wolfvbear@gmail.com>
2015-02-04 19:18:30 +0000

    Added DNS sever parameter.

--- probe/Probe.rb
+++ probe/Probe.rb
@@ -11,8 +11,8 @@ class Probe
         run_silent "ping", "-c", "1", addr
     end
 
-    def host(name)
-        run_silent "host-woods", name
+    def host(name, server)
+        run_silent "host-woods", name, server
     end
 end
 
@@ -20,5 +20,5 @@ p = Probe.new
 #puts p.ping "8.8.8.8"
 #puts p.ping "192.168.0.200"
 
-#puts p.host "google.com"
-#puts p.host "192.168.0.200"
+puts p.host "google.com", "8.8.8.8"
+puts p.host "192.168.0.200", "8.8.8.8"
