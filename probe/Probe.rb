95ccb049ce3a1666444804221cb5b92ef9c8b366
yukipyon <wolfvbear@gmail.com>
2015-02-08 19:19:10 +0000

    Added wget for google and wikipedia.

--- probe/Probe.rb
+++ probe/Probe.rb
@@ -1,4 +1,9 @@
 require 'open3'
+
+# Some webpages return unicode data through wget.
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 class Probe
 
     def run_silent(*args)
@@ -19,4 +24,23 @@ class Probe
         end
     end
 
+    def wget(addr)
+        _, sout, _, t = Open3.popen3 "wget", "-O", "-", addr
+        return sout.read, t.value.success?
+    end
+
+    def http_google()
+        html, ex = wget "google.com"
+        return false unless ex
+        not (html =~ /value="I'm Feeling Lucky"/).nil?
+    end
+
+    def http_wikipedia()
+        
+        html, ex = wget \
+            'https://en.wikipedia.org/w/index.php?title=Furtur&oldid=568297460'
+        puts html
+        return false unless ex
+        not (html =~ /Furfur causes <a href="\/wiki\/Love" title="Love">love<\/a> between a man and a woman/).nil?
+    end
 end
