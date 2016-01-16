0a387df9424c470749db3b38caafaf19b1ed85a7
yukipyon <wolfvbear@gmail.com>
2016-01-16 19:11:12 +0000

    Which command to test availability of binaries.

--- config.rb
+++ config.rb
@@ -1,6 +1,13 @@
+require './cmd/Cmd'
+
+def which? b
+    Cmd.new.run_silent "which", b    
+end
+
 $host = ["host-woods"]
 $ping = ["ping", "-c", "1"]
 $wget = ["wget", "-O", "-"]
 $tor_wget = ["torify"] + $wget
 $crawl = ["wget", "--mirror"]
 $dhcp = ["dhclient"]
+
