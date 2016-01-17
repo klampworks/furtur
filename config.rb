8217b71cc2aa924bb2b5e81b28303701acfa6a99
yukipyon <wolfvbear@gmail.com>
2016-01-17 19:19:55 +0000

    Added some popular alternatives for Gentoo and Debian.

--- config.rb
+++ config.rb
@@ -4,10 +4,17 @@ def cmd_exists? b
     Cmd.new.run_silent "which", b    
 end
 
-$host = ["host-woods"]
-$ping = ["ping", "-c", "1"]
-$wget = ["wget", "-O", "-"]
-$tor_wget = ["torify"] + $wget
-$crawl = ["wget", "--mirror"]
-$dhcp = ["dhclient"]
+def any_cmd cs
+    cs.each do |c| 
+        return c if cmd_exists? c.first
+    end
 
+    ""
+end
+
+$host = any_cmd [["host-woods"], ["host"]]
+$ping = any_cmd [["ping", "-c", "1"]]
+$wget = any_cmd [["wget", "-O", "-"], ["curl"]]
+$tor_wget = any_cmd [["torify"] + $wget ]
+$crawl = any_cmd [["wget", "--mirror"]] 
+$dhcp = any_cmd [["dhcpcd", "dhclient"]]
